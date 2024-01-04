fe59030c003070293ef7efe61b16c665f8110b8b
zhangz <1804919062@qq.com>
2024-01-04 22:50:11 +0800

    更新脚本

--- files/sql/INIT.sql
+++ files/sql/INIT.sql
@@ -1,93 +1,98 @@
 
- 
+DROP TABLE IF EXISTS `good_category`;
+
 
 CREATE TABLE  IF NOT EXISTS good_category (
-                                  id BIGINT(20) PRIMARY KEY,
-                                  isUse BOOLEAN,
-                                  level INT(11),
-                                  name VARCHAR(255),
-                                  paixu INT(11),
-                                  pid INT(11),
-                                  shop_id INT(11),
-                                  user_id INT(11)
-);
- 
+    id BIGINT(20) PRIMARY KEY,
+    isUse BOOLEAN,
+    level INT(11),
+    name VARCHAR(255),
+    paixu INT(11),
+    pid INT(11),
+    shop_id INT(11),
+    user_id INT(11)
+    );
+
+DROP TABLE IF EXISTS `good_info`;
 
 CREATE TABLE IF NOT EXISTS good_info (
-                            after_sale VARCHAR(255),
-                            category_id BIGINT(20),
-                            commission INT(11),
-                            commission_settle_type INT(11),
-                            commission_type INT(11),
-                            commission_user_type INT(11),
-                            date_add VARCHAR(255),
-                            date_update VARCHAR(255),
-                            fx_type INT(11),
-                            got_score INT(11),
-                            got_score_type INT(11),
-                            has_addition BOOLEAN,
-                            has_tour_journey BOOLEAN,
-                            hidden INT(11),
-                            id BIGINT(20),
-                            kanjia BOOLEAN,
-                            kanjia_price INT(11),
-                            limitation BOOLEAN,
-                            logistics_id BIGINT(20),
-                            max_coupons INT(11),
-                            miaosha BOOLEAN,
-                            min_buy_number INT(11),
-                            min_price INT(11),
-                            min_score INT(11),
-                            name VARCHAR(255),
-                            number_fav INT(11),
-                            number_good_reputation INT(11),
-                            number_orders INT(11),
-                            number_reputation INT(11),
-                            number_sells INT(11),
-                            original_price INT(11),
-                            overseas BOOLEAN,
-                            paixu INT(11),
-                            persion INT(11),
-                            pic VARCHAR(255),
-                            pingtuan BOOLEAN,
-                            pingtuan_price INT(11),
-                            property_ids VARCHAR(255),
-                            recommend_status INT(11),
-                            recommend_status_str VARCHAR(255),
-                            seckill_buy_number INT(11),
-                            sell_end BOOLEAN,
-                            sell_start BOOLEAN,
-                            shop_id INT(11),
-                            status INT(11),
-                            status_str VARCHAR(255),
-                            store_alert BOOLEAN,
-                            stores BIGINT(20),
-                            stores0_unsale BOOLEAN,
-                            stores_ext1 INT(11),
-                            stores_ext2 INT(11),
-                            type INT(11),
-                            unit VARCHAR(255),
-                            unuseful_number INT(11),
-                            useful_number INT(11),
-                            user_id INT(11),
-                            vet_status INT(11),
-                            views INT(11),
-                            weight INT(11)
-);
+    after_sale VARCHAR(255),
+    category_id BIGINT(20),
+    commission INT(11),
+    commission_settle_type INT(11),
+    commission_type INT(11),
+    commission_user_type INT(11),
+    date_add VARCHAR(255),
+    date_update VARCHAR(255),
+    fx_type INT(11),
+    got_score INT(11),
+    got_score_type INT(11),
+    has_addition BOOLEAN,
+    has_tour_journey BOOLEAN,
+    hidden INT(11),
+    id BIGINT(20),
+    kanjia BOOLEAN,
+    kanjia_price INT(11),
+    limitation BOOLEAN,
+    logistics_id BIGINT(20),
+    max_coupons INT(11),
+    miaosha BOOLEAN,
+    min_buy_number INT(11),
+    min_price INT(11),
+    min_score INT(11),
+    name VARCHAR(255),
+    number_fav INT(11),
+    number_good_reputation INT(11),
+    number_orders INT(11),
+    number_reputation INT(11),
+    number_sells INT(11),
+    original_price INT(11),
+    overseas BOOLEAN,
+    paixu INT(11),
+    persion INT(11),
+    pic VARCHAR(255),
+    pingtuan BOOLEAN,
+    pingtuan_price INT(11),
+    property_ids VARCHAR(255),
+    recommend_status INT(11),
+    recommend_status_str VARCHAR(255),
+    seckill_buy_number INT(11),
+    sell_end BOOLEAN,
+    sell_start BOOLEAN,
+    shop_id INT(11),
+    status INT(11),
+    status_str VARCHAR(255),
+    store_alert BOOLEAN,
+    stores BIGINT(20),
+    stores0_unsale BOOLEAN,
+    stores_ext1 INT(11),
+    stores_ext2 INT(11),
+    type INT(11),
+    unit VARCHAR(255),
+    unuseful_number INT(11),
+    useful_number INT(11),
+    user_id INT(11),
+    vet_status INT(11),
+    views INT(11),
+    weight INT(11)
+    );
 
 ALTER TABLE good_info ADD content varchar(1000) NULL COMMENT '详细介绍';
+DROP TABLE IF EXISTS `notice`;
 
 
 CREATE TABLE IF NOT EXISTS notice (
-                        id INT(11) PRIMARY KEY,
-                        content VARCHAR(255),
-                        date_add DATE,
-                        is_remind BOOLEAN,
-                        is_show BOOLEAN,
-                        remind_uid INT(11),
-                        title VARCHAR(255),
-                        user_id INT(11)
-);
+    id INT(11) PRIMARY KEY,
+    content VARCHAR(255),
+    date_add DATE,
+    is_remind BOOLEAN,
+    is_show BOOLEAN,
+    remind_uid INT(11),
+    title VARCHAR(255),
+    user_id INT(11)
+    );
+
+DROP TABLE IF EXISTS `shop_info`;
 
 CREATE TABLE IF NOT EXISTS shop_info (
     activity VARCHAR(255),
@@ -132,6 +137,8 @@ CREATE TABLE IF NOT EXISTS shop_info (
     );
 
 -- 商品属性表
+DROP TABLE IF EXISTS `good_property`;
+
 CREATE TABLE IF NOT EXISTS good_property (
     date_add VARCHAR(255),
     id BIGINT PRIMARY KEY,
@@ -156,7 +163,7 @@ CREATE TABLE `order_info` (
                               `goods_number` int(11) DEFAULT NULL,
                               PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
- 
+
 
 -- 订单明细表
 DROP TABLE IF EXISTS `order_good`;
@@ -168,9 +175,9 @@ CREATE TABLE IF NOT EXISTS `order_good` (
     `sku` TEXT,
     `price` decimal(18,2) NOT NULL COMMENT '价格 = number * goods unit price',
     `create_time` VARCHAR(255) NOT NULL,
-    `remark` TEXT,
+    `remark` TEXT
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
- 
+
 ALTER TABLE order_good ADD status TINYINT DEFAULT 0 NOT NULL COMMENT '菜品状态 0 :未下单，1:已经下单,2：制作中，3：已上菜，4 :已支付';
 
 -- 用户表
